7e14dbf4b67f65993f0051ce9a22d1f5dda49a1b
mundix34 <mundix34@gmail.com>
2018-09-30 22:46:36 -0700

    firebase init

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,7 +1,7 @@
-import firebase from 'firebase';
-import 'firebase/firestore';
-import firebaseConfig from './firebaseConfig';
-import { FirebaseApp } from '../../node_modules/@firebase/app-types';
+import firebase from 'firebase'
+import 'firebase/firestore'
+import firebaseConfig from './firebaseConfig'
+import { FirebaseApp } from '../../node_modules/@firebase/app-types'
 const firebase = firebase.initializeApp
 (firebaseConfig)
 export default FirebaseApp.firestore()
