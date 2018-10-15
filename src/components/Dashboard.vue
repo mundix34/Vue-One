627f16b3ff4c99e2285e17e99b9fbd918d3ecf47
mundix34 <mundix34@gmail.com>
2018-10-14 21:26:42 -0700

    brought in data from firebase

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="dashboard">
-        <h3> Dashboard</h3>
-        <div class="fixed-action-btn">
+<ul class="collection with-header">
+    <li class="collection-header"></li></ul>        <div class="fixed-action-btn">
             <router-link to="/new" class="btn-floating btn-large red">
             <i class="fa fa-plus"></i>
 
@@ -12,12 +12,33 @@
     </div>
 </template>
 <script>
+import db from './firebaseInit'
 export default {
     name: 'dashboard',
-    data () {
+    data (){
         return {
+            employees: []
 
         }
+    },
+    created (){
+        db.collection('Employees').get().then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                console.log('testing', doc.id);
+                const data ={
+                    'id': doc.id,
+                    'employee_id': doc.data.employee_id,
+                    'name': doc.data.name,
+                    'dept': doc.data.dept,
+                    'position': doc.data.position
+
+                }
+                this.employees.push(data)
+                
+            })
+
+        })
+
     }
 }
 </script>
--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
 import firebaseConfig from './firebaseConfig'
-import { FirebaseApp } from '../../node_modules/@firebase/app-types'
-const firebase = firebase.initializeApp
+const FirebaseApp = firebase.initializeApp
 (firebaseConfig)
 export default FirebaseApp.firestore()
